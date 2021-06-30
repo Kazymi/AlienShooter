c735778148a22a0259516c208367d84ba8fe693a
Egor Kovel <egor.kovel1@gmail.com>
2021-06-30 13:45:42 +0300

    Fix save data

--- Assets/Scripts/Save/SaveData.cs
+++ Assets/Scripts/Save/SaveData.cs
@@ -1,8 +1,27 @@
 using System.Collections.Generic;
+using UnityEngine;
 
 public class SaveData
 {
-    public int Money;
-    public List<string> UnlockedWeapon  = new List<string>();
-    public string SelectedWeaponName;
+    [SerializeField] private int money;
+    [SerializeField] private List<string> unlockedWeapon = new List<string>();
+    [SerializeField] private string selectedWeaponName;
+
+    public int Money
+    {
+        get => money;
+        set => money = value;
+    }
+
+    public List<string> UnlockedWeapon
+    {
+        get => unlockedWeapon;
+        set => unlockedWeapon = value;
+    }
+
+    public string SelectedWeaponName
+    {
+        get => selectedWeaponName;
+        set => selectedWeaponName = value;
+    }
 }
